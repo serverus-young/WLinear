83d4a04685e5a49722bd787a99ff12895a793e9c
serverus-young <youngdeye4@163.gmail.com>
2025-02-24 14:33:02 +0800

    1

--- model/WFT.py
+++ model/WFT.py
@@ -1,49 +1,52 @@
 import torch
 import torch.nn as nn
 from pytorch_wavelets import DWT1D, IDWT1D  # 一维离散小波变换
+from matplotlib import pyplot as plt
+from torch.nn import MSELoss
 
+from utils.metric  import metric
+import numpy
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 
 class WFT(nn.Module):
     def __init__(self, args):
         super(WFT, self).__init__()
         self.configs = args
-
         # 配置 DWT1D 和 IDWT1D
-        self.cwt = DWT1D(wave='db3', mode='zero' )  # 一维离散小波正变换
+        self.cwt = DWT1D(wave='db3', mode='zero')  # 一维离散小波正变换
         self.icwt = IDWT1D(wave='db3', mode='zero')  # 一维离散小波逆变换
-        self.yh_linear = nn.Linear(args.seq_len//2 + 2, args.pred_len//2 + 2)
-        self.yl_linear = nn.Linear(args.seq_len//2 + 2, args.pred_len//2 + 2)
-    def forward(self, x, x_stamp, y, y_stamp):
-        """
-        前向传播
-        """
-        # x 的维度为 (b, l, n)
-        b, l, n = x.size()
+        self.yl_linear = nn.Linear(args.seq_len//2 + 2, args.d_model//2 + 2)
+        self.yh_linear = nn.Linear(args.seq_len//2 + 2, args.d_model//2 + 2)
+        self.proj = nn.Linear(args.d_model, args.pred_len )
+        # self.wavecore = Waveletcore(args.seq_len//2 + 2, args.pred_len//2 + 2)
 
-        # 数据标准化
+    def forward(self, x, x_stamp, y, y_stamp):
         mean = x.mean(dim=1, keepdim=True)  # 按时间维度求均值
         x = x - mean
         stdev = torch.sqrt(torch.var(x, dim=1, keepdim=True) + 1e-5)
         x /= stdev
 
-        # 调整输入形状为 (b, n, l) 以适配 DWT1D
-        x = x.permute(0, 2, 1)  # 将形状从 (b, l, n) 变为 (b, n, l)
+        yl, yh_ = self.cwt(x.permute(0,2,1))
+        yh = yh_[0]
+        # plt.figure(figsize=(10, 5))
+        # plt.subplot(3, 1, 1)
+        # plt.plot(yl.cpu().detach().numpy()[0, 0, :], color='g')
+        # plt.subplot(3, 1, 2)
+        # plt.plot(yh1.cpu().detach().numpy()[0, 0, :], color='g')
+        # plt.subplot(3, 1, 3)
+        # plt.plot(yh2.cpu().detach().numpy()[0, 0, :], color='g')
+        # plt.show()
 
-        # 小波正变换
-        Yl, Yh = self.cwt(x)  # 返回低频分量 (Yl) 和高频分量 (Yh)
-
-        # 展平高频分量并输入线性层
-        yh = Yh[0]
-        yh_pred = self.yh_linear(yh)
-        Yl_pred = self.yl_linear(Yl)
-        # 逆变换
-        Yh_pred = []
-        Yh_pred.append(yh_pred)
-        pred = self.icwt((Yl_pred, Yh_pred))  # 逆变换后的形状为 (b, n, l)
-
-        # 恢复形状为 (b, l, n)
+        yl_pred = self.yl_linear(yl)
+        yh_pred_ = self.yh_linear(yh)
+        yh_pred = []
+        yh_pred.append(yh_pred_)
+        pred = self.icwt((yl_pred, yh_pred))
+        pred =self.proj(pred)
         pred = pred.permute(0, 2, 1)
-
-        # 还原标准化
         pred = pred * stdev + mean
         return pred
--- model/WT2.py
+++ model/WT2.py
@@ -1,19 +1,21 @@
 import torch
 import torch.nn as nn
 from pytorch_wavelets import DWT1D, IDWT1D  # 一维离散小波变换
+from matplotlib import pyplot as plt
+from torch.nn import MSELoss
 
+from utils.metric  import metric
+import numpy
 class WT2(nn.Module):
     def __init__(self, args):
         super(WT2, self).__init__()
         self.configs = args
-
         # 配置 DWT1D 和 IDWT1D
-        self.cwt = DWT1D(wave='db3', mode='zero')  # 一维离散小波正变换
+        self.cwt = DWT1D(wave='db3', mode='zero' )  # 一维离散小波正变换
         self.icwt = IDWT1D(wave='db3', mode='zero')  # 一维离散小波逆变换
         self.yh_linear = nn.Linear(args.seq_len//2 + 2, args.pred_len//2 + 2)
         self.yl_linear = nn.Linear(args.seq_len//2 + 2, args.pred_len//2 + 2)
-
-    def forward(self, x, x_stamp, y, y_stamp, mask_prob=0.1):
+    def forward(self, x, x_stamp, y, y_stamp):
         """
         前向传播
         """
@@ -31,20 +33,16 @@ class WT2(nn.Module):
 
         # 小波正变换
         Yl, Yh = self.cwt(x)  # 返回低频分量 (Yl) 和高频分量 (Yh)
-
-        # 展平高频分量并输入线性层
-        yh = Yh[0]
+        yh = Yh[0]        # 展平高频分量并输入线性层
         yh_pred = self.yh_linear(yh)
         Yl_pred = self.yl_linear(Yl)
+
+
         # 逆变换
         Yh_pred = []
         Yh_pred.append(yh_pred)
         pred = self.icwt((Yl_pred, Yh_pred))  # 逆变换后的形状为 (b, n, l)
 
-        # 恢复形状为 (b, l, n)
-        pred = pred.permute(0, 2, 1)  # 变为 (b, self.pred_len, n)
-
-        # 还原标准化
+        pred = pred.permute(0, 2, 1)
         pred = pred * stdev + mean
-
         return pred
